f81d90a045eab930f563de1f771a9feedc1f4e13
seren5 <shuyueeren@gmail.com>
2024-10-12 01:53:11 +0200

    client added for basic math server

--- client.py
+++ client.py
@@ -23,14 +23,55 @@ def encode_message(message):
     # Add an application-layer header to the message that the VPN can use to forward it
     return f"{SERVER_IP}|{SERVER_PORT}|{message}" # Combines server information with the message
 
+
 print("client starting - connecting to VPN at IP", VPN_IP, "and port", VPN_PORT)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((VPN_IP, VPN_PORT))
-    print(f"connection established, sending message '{encode_message(MSG)}'")
-    MSG = encode_message(MSG)
-    s.sendall(bytes(MSG, 'utf-8'))
-    print("message sent, waiting for reply")
-    data = s.recv(1024).decode("utf-8")
+
+exit_loop = False
+while exit_loop == False:
+    connection_choice = input("would you like to connect to basic math or other? (1 for basic math, 2 for other)    ")
+    if connection_choice == "1":
+        exit_loop_2 = False
+        while exit_loop_2 == False:
+            msg = input("What (addition/subtraction) equation would you like solved? (Please enter your equation in the following format: +1:3:5 or -1:3:5. The order of the numbers matter.)   ") # Asks for user input
+            if msg == "quit": # If the user inputs quit, the program exits
+                print("client quitting at operator request")
+                exit(0)
+            elif msg == "": # If the user inputs an empty message, they are prompted to put in numbers again
+                print("You have not specified a value, please try again.")
+            elif msg.startswith("+") or msg.startswith("-"):
+                if ":" in msg and "0" in msg or "1" in msg or "2" in msg or "3" in msg or "4" in msg or "5" in msg or "6" in msg or "7" in msg or "8" in msg or "9" in msg: # Checks for ":" or number (it isn't the prettiest but I couldn't get it to work in time)
+                    break
+                else: # The user has input a letter or did not start with "+" or "-"
+                        print("Please format your equation in the following format:+1:3:5 or -1:3:5")
+            else: # The user has input a letter or did not start with "+" or "-"
+                    print("You have not formatted your equation properly.")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((VPN_IP, VPN_PORT))
+            print(f"connection established, sending message '{encode_message(msg)}'")
+            MSG = encode_message(MSG)
+            s.sendall(bytes(MSG, 'utf-8'))
+            print("message sent, waiting for reply")
+            data = s.recv(1024).decode("utf-8")
+    elif connection_choice == "2":
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((VPN_IP, VPN_PORT))
+            print(f"connection established, sending message '{encode_message(MSG)}'")
+            MSG = encode_message(MSG)
+            s.sendall(bytes(MSG, 'utf-8'))
+            print("message sent, waiting for reply")
+            data = s.recv(1024).decode("utf-8")
+            break
+    else:
+        print("you have not selected a choice. please try again.")
+     
+    
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((VPN_IP, VPN_PORT))
+#     print(f"connection established, sending message '{encode_message(MSG)}'")
+#     MSG = encode_message(MSG)
+#     s.sendall(bytes(MSG, 'utf-8'))
+#     print("message sent, waiting for reply")
+#     data = s.recv(1024).decode("utf-8")
 
 print(f"Received response: '{data}' [{len(data)} bytes]")
 print("client is done!")
